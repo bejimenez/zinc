05bb8fe542bd983695572cfc442ef6313c90cd05
bejimenez <be.jimenez666@gmail.com>
2025-06-16 07:29:36 -0700

    added dark mode with orange accent theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import EditorView from './components/editor/EditorView';
 import GameView from './components/game/GameView';
 import { Button } from './components/ui/button';
 
-
 // We'll define a type for our script data
 export type ScriptData = {
   nodes: Node[];
@@ -26,20 +25,20 @@ function App() {
   };
 
   return (
-    <div className="w-screen h-screen bg-background text-foreground">
-      {isEditorMode ? (
-        <EditorView onPlay={handlePlay} />
-      ) : (
-        <div>
-          <div className="absolute top-2 left-2 z-10">
-            <Button onClick={handleStop} variant="destructive">
-              Stop
-            </Button>
+      <div className="w-screen h-screen bg-background text-foreground">
+        {isEditorMode ? (
+          <EditorView onPlay={handlePlay} />
+        ) : (
+          <div>
+            <div className="absolute top-2 left-2 z-10">
+              <Button onClick={handleStop} variant="destructive">
+                Stop
+              </Button>
+            </div>
+            <GameView scriptData={scriptData} />
           </div>
-          <GameView scriptData={scriptData} />
-        </div>
-      )}
-    </div>
+        )}
+      </div>
   );
 }
 
